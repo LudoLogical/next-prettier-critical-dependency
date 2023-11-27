b8d1933e1ab93bd1965f6beb55025d715849a8ed
LudoLogical <hello@danieldeanda.com>
2023-11-26 16:05:35 -0800

    Issue reproduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
-/** Add your relevant code here for the issue to reproduce */
+"use client";
+
+import { useEffect } from "react";
+import prettier from "prettier";
+import babelPlugin from "prettier/plugins/babel";
+import estreePlugin from "prettier/plugins/estree";
+
 export default function Home() {
-  return null
+  useEffect(() => {
+    const makePrettier = async () => {
+      console.log(
+        await prettier.format(
+          "export default function Test(){return(<p>hi</p>);}",
+          { parser: "babel", plugins: [babelPlugin, estreePlugin] }
+        )
+      );
+    };
+    makePrettier();
+  }, []);
+  return null;
 }
